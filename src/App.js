c56e91c85a3449be47e66159132a3df583cd46a5
Ieva Kliorikaityte <ievakliorikaityte@Ievas-MacBook-Pro-2.local>
2021-05-20 10:10:39 +0300

    commit5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState as state, useEffect as effect} from 'react'
+import http from "./plugins/Fetch"
+import Upload from "./components/Upload";
+import Product from "./components/Product";
 
 function App() {
+  const [products, setProducts] = state([])
+
+  effect(() => {
+    http.get('/all').then(res => {
+      setProducts(res.products)
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Upload set={setProducts}/>
+        <div className="m-20">
+          {products.map((prod, index) =>
+              <Product key={index}
+                       prod={prod}
+                       set={setProducts}
+              />
+          )}
+        </div>
+      </div>
   );
 }
 
